7f4ba38dae7e86b9615fc3da0a557e0a04420df5
Trishia <mariatrishia2001@gmail.com>
2024-10-17 02:53:47 +0800

    10-17-2024 - Books

--- finance_management_system/fms/views/views.py
+++ finance_management_system/fms/views/views.py
@@ -4,7 +4,7 @@ from ..models.coa_model import ChartOfAccounts
 
 def chart_of_accounts(request):
     accounts = ChartOfAccounts.objects.all().order_by('account_code')
-    return render(request, 'coa/coa_table.html', {'accounts': accounts})
+    return render(request, 'coa/coa_tab_panel.html', {'accounts': accounts})
 
 def manage_books(request):
     return render(request, 'bm/bm_tab_panel.html')
