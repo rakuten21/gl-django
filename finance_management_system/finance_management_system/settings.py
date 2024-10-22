e94beb4e0c4336139e94c20a6a79c8bfd9b1b591
Trishia <mariatrishia2001@gmail.com>
2024-10-22 15:06:34 +0800

    settings

--- finance_management_system/finance_management_system/settings.py
+++ finance_management_system/finance_management_system/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'finance_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-database_url = os.environ.get("DATABASE_URL")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,8 +95,8 @@ DATABASES = {
     # }
 }
 
+database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
